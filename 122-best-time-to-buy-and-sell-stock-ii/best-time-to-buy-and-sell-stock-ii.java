3c605c7ee69fdbab48c2a85bc38b272cddbbd417
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-03-31 18:20:10 +0530

    Time: 3 ms (10.00%) | Memory: 49.4 MB (5.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,17 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int bd = 0;
-        int sd = 0;
-        int profit = 0;
-        for(int i = 1; i< prices.length; i++){
-            if(prices[i] >= prices[i-1]){
-                sd++;
-            }else{
-                profit += prices[sd]-prices[bd];
-                bd = sd = i;
-            }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        profit += prices[sd]-prices[bd];
+        if (n == 0) return 0;
+        return getMaximumProfitUtil(prices, 0, 0, n, dp);
+    }
+
+    static int getMaximumProfitUtil(int[] Arr, int ind, int buy, int n, int[][] dp) {
+        if (ind == n) return 0;
+        if (dp[ind][buy] != -1)
+            return dp[ind][buy];
+
+        int profit;
+        if (buy == 0) { // We can buy the stock
+            profit = Math.max(0 + getMaximumProfitUtil(Arr, ind + 1, 0, n, dp),
+                    -Arr[ind] + getMaximumProfitUtil(Arr, ind + 1, 1, n, dp));
+        } else { // We can sell the stock
+            profit = Math.max(0 + getMaximumProfitUtil(Arr, ind + 1, 1, n, dp),
+                    Arr[ind] + getMaximumProfitUtil(Arr, ind + 1, 0, n, dp));
+        }
+        dp[ind][buy] = profit;
         return profit;
     }
 }
