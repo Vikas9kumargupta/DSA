574fac5247aec1ab2698ee050fc333f544e3d9b1
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-03-31 09:40:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (23.98%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
 
-        //Tabulation
+        //Space Optimization
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
+        int prev2 = 0;
+        int prev1 = nums[0];
         for(int i = 1; i<n; i++){
             int pick = nums[i];
             if(i>1){
-                pick += dp[i-2];  
+                pick += prev2;  
             }
-            int unpick = dp[i-1];
-            dp[i] = Math.max(pick, unpick);
+            int unpick = prev1;
+            int curr = Math.max(pick, unpick);
+            prev2 = prev1;
+            prev1 = curr;
+
         }
-        return dp[n-1];
+        return prev1;
     }
 }
