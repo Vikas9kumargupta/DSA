9a655c9f7041b25fb91877e497042600f0072e4d
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-03-31 09:48:10 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (63.95%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        // If only one house, return its value
         if (n == 1) return nums[0]; 
-        
-        return Math.max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+        int left =  robLinear(Arrays.copyOfRange(nums, 0 , n-1));
+        int right = robLinear(Arrays.copyOfRange(nums, 1 , n));
+        return Math.max(left, right);
     }
 
-    private int robLinear(int[] nums, int start, int end) {
-        int prev1 = 0, prev2 = 0;
-
-        for (int i = start; i <= end; i++) {
-            int temp = prev1;
-            prev1 = Math.max(prev1, prev2 + nums[i]);
-            prev2 = temp;
+    private int robLinear(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 2; i <= n; i++ ) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
         }
-
-        return prev1;
+        return dp[n];
     }
 }
