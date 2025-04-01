b864db61207a638fc324acbfa014a18b28cce118
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-04-01 12:11:55 +0530

    Time: 2 ms (80.27%) | Memory: 41.9 MB (79.86%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][] ahead = new int[2][k+1]; 
+        int[][] curr = new int[2][k+1];  
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy == 0) {
+                        curr[buy][cap] = Math.max(ahead[0][cap], -prices[ind] + ahead[1][cap]);
+                    } else {
+                        curr[buy][cap] = Math.max(ahead[1][cap], prices[ind] + ahead[0][cap - 1]);
+                    }
+                }
+            }
+            ahead = curr.clone();
+        }
+        return ahead[0][k];
+    }
+}
