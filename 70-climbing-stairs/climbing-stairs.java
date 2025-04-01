441463a867f157de86cb92f397bc2098efe72632
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-04-01 10:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (96.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,13 +3,13 @@ class Solution {
         if(n==1 || n== 2){
             return n;
         }
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        int fS = 1;
+        int sS = 2;
         for(int i = 3; i<= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int cS = fS + sS;
+            fS = sS;
+            sS = cS;
         }
-        return dp[n];
+        return sS;
     }
 }
