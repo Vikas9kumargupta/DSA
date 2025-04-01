a0be084dcc47ae8d16b20b2bfb407f18517b4586
Vikas Kumar Gupta <104565684+Vikas9kumargupta@users.noreply.github.com>
2025-04-01 10:33:34 +0530

    Time: 1 ms (19.91%) | Memory: 42.3 MB (11.94%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if(grid[m-1][n-1]==1) return 0;
-        int[][] dp = new int[m][n];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        return rec(0,0,grid,m-1,n-1,dp);
-    }
-     private int rec(int r,int c,int[][] grid,int m,int n,int[][] dp)
-    {
-        if(r==m && c==n) return 1;
-        if(r>m || c>n || (grid[r][c]==1)) return 0;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int right = rec(r,c+1,grid,m,n,dp);
-        int down = rec(r+1,c,grid,m,n,dp);
-        return dp[r][c] = right + down;
-    }
+        int n = grid.length;
+        int m = grid[0].length;
+        int dp[][] = new int[n][m];
+        if (grid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) { 
+                    dp[i][j] = 0; 
+                    continue;
+                }
+                if (i == 0 && j == 0) continue;
+                
+                int up = 0;
+                int left = 0;
 
+                if (i > 0) up = dp[i - 1][j];
+                if (j > 0) left = dp[i][j - 1];
+
+                dp[i][j] += up + left;
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
 }
     
